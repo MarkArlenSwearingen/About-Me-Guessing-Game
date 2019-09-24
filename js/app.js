2f5b38786cde6cf8e6cbe3081511a3bfdd332137
Mark Swearingen <markswearingen@bendcable.com>
2019-09-24 14:17:48 -0700

    Prompt, alert and console log working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 'use strict'
+
+var userName = prompt('Please enter your name');
+console.log(userName);
+alert('Hello ' + userName + '. Welcome to the Mark Swearingen webpage');
+
+var guessSchool = prompt('Did Mark Swearingen attend the University of Oregon?  Please answer either yes or no');
+console.log(guessSchool);
+
