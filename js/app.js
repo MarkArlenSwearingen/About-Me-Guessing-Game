b7ee5a14104fd1b96eb7354024dd8f6443c84a38
Mark Swearingen <markswearingen@bendcable.com>
2019-09-25 19:28:26 -0700

    Added reset.css to HTML and styling to style.css

--- js/app.js
+++ js/app.js
@@ -10,104 +10,104 @@ alert('Hello ' + userName + '. Welcome to the Mark Swearingen webpage');
 // set variable for tracking total number of correct answers
 var correctAnswers = 0;
 
-var guessSchool = prompt('Did Mark Swearingen attend the University of Oregon?  Please answer either yes or no');
+// var guessSchool = prompt('Did Mark Swearingen attend the University of Oregon?  Please answer either yes or no');
 
-if (guessSchool.toLowerCase() === 'yes') {
-  alert('Congratulations, yes is correct.  Although Mark graduated from Clarement McKenna College, he also attended the University of Oregon his sophomore year and winter term senior year')
-  correctAnswers++;
-} else {
-  alert('Sorry, no is incorrect.  Although Mark graduated from Clarement McKenna College, he also attended the University of Oregon his sophomore year and winter term senior year')
-}
+// if (guessSchool.toLowerCase() === 'yes') {
+//   alert('Congratulations, yes is correct.  Although Mark graduated from Clarement McKenna College, he also attended the University of Oregon his sophomore year and winter term senior year')
+//   correctAnswers++;
+// } else {
+//   alert('Sorry, no is incorrect.  Although Mark graduated from Clarement McKenna College, he also attended the University of Oregon his sophomore year and winter term senior year')
+// }
 
-var guessSportOregon = prompt('Did Mark Swearingen skipper on the University of Oregon sailing team?  Please answer either yes or no');
+// var guessSportOregon = prompt('Did Mark Swearingen skipper on the University of Oregon sailing team?  Please answer either yes or no');
 
-if (guessSportOregon.toLowerCase() === 'yes') {
-  alert('Congratulations, yes is correct.  Mark raced for the University of Oregon his sophomore year in regattas in Washington, Oregon and British Columbia');
-  correctAnswers++;  
-} else {
-  alert('Sorry, no is incorrect.  Mark raced for the University of Oregon his sophomore year in regattas in Washington, Oregon and British Columbia');
-}
+// if (guessSportOregon.toLowerCase() === 'yes') {
+//   alert('Congratulations, yes is correct.  Mark raced for the University of Oregon his sophomore year in regattas in Washington, Oregon and British Columbia');
+//   correctAnswers++;  
+// } else {
+//   alert('Sorry, no is incorrect.  Mark raced for the University of Oregon his sophomore year in regattas in Washington, Oregon and British Columbia');
+// }
 
-var guessSportCMC = prompt('Did Mark Swearingen race cross country at Clarement McKenna College?  Please answer either yes or no');
+// var guessSportCMC = prompt('Did Mark Swearingen race cross country at Clarement McKenna College?  Please answer either yes or no');
 
-if (guessSportCMC.toLowerCase() === 'yes') {
-  alert('Sorry, yes is incorrect.  Mark actually pole vaulted for Claremont McKenna College but did not run cross country');
-} else {
-  correctAnswers++;
-  alert('Congratulations, no is correct.  Mark actually pole vaulted for Claremont McKenna College but did not run cross country');
-}
+// if (guessSportCMC.toLowerCase() === 'yes') {
+//   alert('Sorry, yes is incorrect.  Mark actually pole vaulted for Claremont McKenna College but did not run cross country');
+// } else {
+//   correctAnswers++;
+//   alert('Congratulations, no is correct.  Mark actually pole vaulted for Claremont McKenna College but did not run cross country');
+// }
 
-var guessLocal = prompt('Did Mark Swearingen ever live in San Francisco?  Please answer either yes or no');
+// var guessLocal = prompt('Did Mark Swearingen ever live in San Francisco?  Please answer either yes or no');
 
-if (guessLocal.toLowerCase() === 'yes') {
-  alert('Congratulations, yes is correct.  Mark lived in San Francisco before taking a job in Eugene, Oregon for a Computer Aided Manufacturing(CAM) software development company');
-  correctAnswers++;
-} else {
-  alert('Sorry, no is incorrect.  Mark lived in San Francisco before taking a job in Eugene, Oregon for a Computer Aided Manufacturing(CAM) software development company');
-}
+// if (guessLocal.toLowerCase() === 'yes') {
+//   alert('Congratulations, yes is correct.  Mark lived in San Francisco before taking a job in Eugene, Oregon for a Computer Aided Manufacturing(CAM) software development company');
+//   correctAnswers++;
+// } else {
+//   alert('Sorry, no is incorrect.  Mark lived in San Francisco before taking a job in Eugene, Oregon for a Computer Aided Manufacturing(CAM) software development company');
+// }
 
-var guessOccupation = prompt('Is Mark Swearingen\'s occupational goal to be a talented Software Developer?  Please answer either yes or no');
+// var guessOccupation = prompt('Is Mark Swearingen\'s occupational goal to be a talented Software Developer?  Please answer either yes or no');
 
-if (guessOccupation.toLowerCase() === 'yes') {
-  alert('Congratulations, yes is correct.  Mark is currently working towards becoming a talented Software Developer at COCC DevStation');
-  correctAnswers++;
-} else {
-  alert('Sorry, no is incorrect.  Mark is currently working towards becoming a talented Software Developer at COCC DevStation');
-}
+// if (guessOccupation.toLowerCase() === 'yes') {
+//   alert('Congratulations, yes is correct.  Mark is currently working towards becoming a talented Software Developer at COCC DevStation');
+//   correctAnswers++;
+// } else {
+//   alert('Sorry, no is incorrect.  Mark is currently working towards becoming a talented Software Developer at COCC DevStation');
+// }
 
 
-// Code for guessing number game.
-// Stretch goal will be to add a random number rather than a favorite number.
-var guessNumber = parseInt(prompt('Please guess Mark\'s favorite number'), 10);
-console.log(guessNumber);
+// // Code for guessing number game.
+// // Stretch goal will be to add a random number rather than a favorite number.
+// var guessNumber = parseInt(prompt('Please guess Mark\'s favorite number'), 10);
+// console.log(guessNumber);
 
-var favoriteNumber = 213;
-console.log(favoriteNumber);
+// var favoriteNumber = 213;
+// console.log(favoriteNumber);
 
-for (var i = 0; i < 4; i++) {
-  if (guessNumber === favoriteNumber) {
-    correctAnswers++;
-    alert('You guess correctly. The number is ' + favoriteNumber);
-    i = i + 4;
-  }
-  if (guessNumber < favoriteNumber){
-    alert('Your guess of ' + guessNumber + 'was too low.  Please guess higher');
-  }
-  if (guessNumber > favoriteNumber){
-    alert('Your guess of ' + guessNumber + 'was too high.  Please guess lower');
-    // guessNumber = parseInt(prompt('Your guess of ' + guessNumber + 'was too high.  Please guess lover'), 10);
-  }
-  if (i === 3) {
-    alert ('Your number of guesses had ended.  The number is ' + favoriteNumber);
-  } else {
-    if (guessNumber !== favoriteNumber) {
-      guessNumber = parseInt(prompt('Please guess again'), 10);
-    }
-  }
-}
+// for (var i = 0; i < 4; i++) {
+//   if (guessNumber === favoriteNumber) {
+//     correctAnswers++;
+//     alert('You guess correctly. The number is ' + favoriteNumber);
+//     i = i + 4;
+//   }
+//   if (guessNumber < favoriteNumber){
+//     alert('Your guess of ' + guessNumber + 'was too low.  Please guess higher');
+//   }
+//   if (guessNumber > favoriteNumber){
+//     alert('Your guess of ' + guessNumber + 'was too high.  Please guess lower');
+//     // guessNumber = parseInt(prompt('Your guess of ' + guessNumber + 'was too high.  Please guess lover'), 10);
+//   }
+//   if (i === 3) {
+//     alert ('Your number of guesses had ended.  The number is ' + favoriteNumber);
+//   } else {
+//     if (guessNumber !== favoriteNumber) {
+//       guessNumber = parseInt(prompt('Please guess again'), 10);
+//     }
+//   }
+// }
 
-// Code for guessing from an array
+// // Code for guessing from an array
 
-var favoriteMascotsArray = ['DUCK', 'DUCKS','BEAVER', 'BEAVERS', 'TIGER', 'TIGERS', 'BEAR', 'BEARS', 'EAGLE', 'EAGLES', '49ER', '49ERS', 'STAG', 'STAGS', 'ATHENA', 'ATHENAS', 'PHANATIC', 'PHANATICS'];
-console.log (favoriteMascotsArray);
+// var favoriteMascotsArray = ['DUCK', 'DUCKS','BEAVER', 'BEAVERS', 'TIGER', 'TIGERS', 'BEAR', 'BEARS', 'EAGLE', 'EAGLES', '49ER', '49ERS', 'STAG', 'STAGS', 'ATHENA', 'ATHENAS', 'PHANATIC', 'PHANATICS'];
+// console.log (favoriteMascotsArray);
 
-for (var i = 0; i < 6; i++) {
-  var mascotGuess = prompt('Please guess one of my favorite mascots');
-  if (favoriteMascotsArray.includes(mascotGuess.toUpperCase())){
-    console.log(mascotGuess);
-    correctAnswers++;
-    alert('Correct! My favorit mascots are ' + favoriteMascotsArray);
-    i = 6;
-    //set counter of correct answers
-  } else {
-    if (i === 5){
-      alert('The number of guesses has been reached. My favorite mascots are ' + favoriteMascotsArray);
-    }
-  }
-}
+// for (var i = 0; i < 6; i++) {
+//   var mascotGuess = prompt('Please guess one of my favorite mascots');
+//   if (favoriteMascotsArray.includes(mascotGuess.toUpperCase())){
+//     console.log(mascotGuess);
+//     correctAnswers++;
+//     alert('Correct! My favorit mascots are ' + favoriteMascotsArray);
+//     i = 6;
+//     //set counter of correct answers
+//   } else {
+//     if (i === 5){
+//       alert('The number of guesses has been reached. My favorite mascots are ' + favoriteMascotsArray);
+//     }
+//   }
+// }
 
-console.log(correctAnswers);
+// console.log(correctAnswers);
 
-//  Final message thanking the user, by name provided, for playing the game.
-alert('Thank you ' + userName + ' for answering the questions.  You got ' + correctAnswers + ' correct answers out of seven questions including the number game. Hopefully you learned some interesting information about Mark Swearingen and had some fun.');
+// //  Final message thanking the user, by name provided, for playing the game.
+// alert('Thank you ' + userName + ' for answering the questions.  You got ' + correctAnswers + ' correct answers out of seven questions including the number game. Hopefully you learned some interesting information about Mark Swearingen and had some fun.');
 
